c7c5f45e5336ce663eaf233c7b1884c5009093e2
marinzilic7 <marinzilic507@gmail.com>
2025-02-20 15:49:43 +0100

    dodavanje google fonta za projekt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,9 +2,15 @@ export default defineNuxtConfig({
   app:{
     head:{
       title:"FitZone", 
+      link:[
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=Libre+Franklin:ital,wght@0,100..900;1,100..900&family=Mulish:ital,wght@0,200..1000;1,200..1000&display=swap'
+        }
+      ]
     }
   },
-  css: ["@/assets/styles.scss", "bootstrap/dist/css/bootstrap.min.css"],
+  css: [ "bootstrap/dist/css/bootstrap.min.css","@/assets/styles.scss",],
   plugins: [{ src: "~/plugins/bootstrap.js", mode: "client" }],
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
