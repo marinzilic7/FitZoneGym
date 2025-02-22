87a539f69e84f8ea143e0b969c596b90820a020b
marinzilic7 <marinzilic507@gmail.com>
2025-02-23 00:36:00 +0100

    rad na galeriji

--- components/Gallery.vue
+++ components/Gallery.vue
@@ -1,3 +1,64 @@
+<script setup>
+const images = [
+  {
+    src: "/images/gallery/gallery.jpg",
+    alt: "gallery1",
+    height: "400px",
+  },
+  {
+    src: "images/gallery/gallery2.jpg",
+    alt: "gallery2",
+    width: "500px",
+  },
+  {
+    src: "images/gallery/gallery3.jpg",
+    alt: "gallery3",
+    width: "300px",
+  },
+  {
+    src: "images/gallery/gallery4.jpg",
+    alt: "gallery4",
+    width: "400px",
+  },
+  {
+    src: "images/gallery/gallery7.jpg",
+    alt: "gallery4",
+    height: "350px",
+  },
+  {
+    src: "images/gallery/gallery5.jpg",
+    alt: "gallery5",
+    width: "350px",
+  },
+  {
+    src: "images/gallery/gallery8.jpg",
+    alt: "gallery5",
+    width: "350px",
+  },
+  {
+    src: "images/gallery/gallery10.jpg",
+    alt: "gallery5",
+    width: "450px",
+  },
+  {
+    src: "images/gallery/gallery11.jpg",
+    alt: "gallery5",
+    width: "300px",
+  },
+  {
+    src: "images/gallery/gallery9.jpg",
+    alt: "gallery5",
+    height: "350px",
+  },
+];
+
+const selectedImage = ref(null);
+
+const setSelectedImage = (image) => {
+  selectedImage.value = image;
+};
+</script>
+
 <template>
   <div class="main-gallery mt-5 p-5">
     <div class="gallery-items">
@@ -6,63 +67,57 @@
         class="gallery d-flex justify-content-center flex-wrap align-items-center"
         data-aos="fade-right"
       >
-        <img
-          src="@/public/images/gallery/gallery.jpg"
-          alt="gallery1"
-          height="400px"
-        />
-        <img
-          src="@/public/images/gallery/gallery2.jpg"
-          alt="gallery2"
-          width="500px"
-        />
-        <img
-          src="@/public/images/gallery/gallery3.jpg"
-          alt="gallery3"
-          width="300px"
-        />
-        <img
-          src="@/public/images/gallery/gallery4.jpg"
-          alt="gallery4"
-          width="400px"
-        />
-        <img
-          src="@/public/images/gallery/gallery7.jpg"
-          alt="gallery4"
-          height="350px"
-        />
-
-        <img
-          src="@/public/images/gallery/gallery5.jpg"
-          alt="gallery5"
-          width="350px"
-        />
-
-        <img
-          src="@/public/images/gallery/gallery8.jpg"
-          alt="gallery5"
-          width="350px"
-        />
-        <img
-          src="@/public/images/gallery/gallery10.jpg"
-          alt="gallery5"
-          width="450px"
-        />
-        <img
-          src="@/public/images/gallery/gallery11.jpg"
-          alt="gallery5"
-          width="300px"
-        />
-        <img
-          src="@/public/images/gallery/gallery9.jpg"
-          alt="gallery5"
-          height="350px"
-        />
+        <div v-for="(image, index) in images" :key="index">
+          <img
+            data-bs-toggle="modal"
+            data-bs-target="#exampleModal"
+            data-bs-whatever="@mdo"
+            :src="image.src"
+            :alt="image.alt"
+            :width="image.width"
+            :height="image.height"
+            @click="setSelectedImage(image)"
+          />
+        </div>
       </div>
       <div class="d-flex justify-content-center align-items-center">
         <button class="btn">Show More</button>
       </div>
       "
+    </div>
+  </div>
+
+  <!-- Modal  -->
+
+  <div
+    class="modal fade"
+    id="exampleModal"
+    tabindex="-1"
+    aria-labelledby="exampleModalLabel"
+    aria-hidden="true"
+  >
+    <div class="modal-dialog modal-fullscreen">
+      <div class="modal-content bg-transparent border-0">
+        <div
+          class="modal-body p-0 d-flex align-items-center justify-content-center"
+        >
+          <div class="img-btn-x position-relative">
+            <img
+              v-if="selectedImage"
+              :src="selectedImage.src"
+              :alt="selectedImage.alt"
+              height="700px"
+              width="100%"
+            />
+            <button
+              type="button"
+              class="btn-close btn-close-white z-3 position-absolute top-0 end-0 m-3"
+              data-bs-dismiss="modal"
+              aria-label="Close"
+            ></button>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -87,8 +142,15 @@
       border-radius: 20px;
       margin: 10px;
       opacity: 0.9;
+      transition: all 0.3s;
+      cursor: pointer;
+      &:hover {
+        border: 1.5px solid $button-color;
+        transform: scale(1.1);
+      }
     }
   }
+ 
 }
 
 @media screen and (max-width: 1240px) {
@@ -202,6 +264,11 @@
         width: auto;
         height: 150px;
       }
+    }
+  }
+  .img-btn-x {
+    img {
+      height: 350px;
     }
   }
 }
