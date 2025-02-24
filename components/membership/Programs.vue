2be14d13d36e0828e3d56a6827b10e98fc603407
marinzilic7 <marinzilic507@gmail.com>
2025-02-24 21:33:15 +0100

    rad na membership

---
+++ components/membership/Programs.vue
@@ -0,0 +1,107 @@
+<template>
+  <div class="main-programs d-flex justify-content-evenly flex-wrap align-items-center mt-5">
+    <div
+      class="programs p-5 d-flex flex-column justify-content-center align-items-center gap-5"
+    >
+      <div>
+        <h3>Basic</h3>
+        <h3>50%/Month</h3>
+      </div>
+      <div>
+        <ul>
+          <li class="text-light">Cost-Effective Option</li>
+          <li class="text-light">Limited Amentities</li>
+          <li class="text-light">Group Fitness Classes</li>
+          <li class="text-light">Locker Room</li>
+        </ul>
+      </div>
+      <button class="btn btn-warning btn-sm">Buy Now</button>
+    </div>
+    <div
+      class="programs p-5 d-flex flex-column justify-content-center align-items-center gap-5"
+    >
+      <div>
+        <h3>Basic</h3>
+        <h3>50%/Month</h3>
+      </div>
+      <div>
+        <ul>
+          <li class="text-light">Cost-Effective Option</li>
+          <li class="text-light">Limited Amentities</li>
+          <li class="text-light">Group Fitness Classes</li>
+          <li class="text-light">Locker Room</li>
+        </ul>
+      </div>
+      <button class="btn btn-warning btn-sm">Buy Now</button>
+    </div>
+    <div
+      class="programs p-5 d-flex flex-column justify-content-center align-items-center gap-5"
+    >
+      <div>
+        <h3>Basic</h3>
+        <h3>50%/Month</h3>
+      </div>
+      <div>
+        <ul>
+          <li class="text-light">Cost-Effective Option</li>
+          <li class="text-light">Limited Amentities</li>
+          <li class="text-light">Group Fitness Classes</li>
+          <li class="text-light">Locker Room</li>
+        </ul>
+      </div>
+      <button class="btn btn-warning btn-sm">Buy Now</button>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@import "@/assets/scss/variables";
+
+.programs {
+  background-color: $dark-color;
+  transition: all 0.3s ease-in-out;
+  border-radius: 30px;
+  cursor: pointer;
+  &:hover {
+    outline: 1px solid $button-color;
+    transform: scale(1.05);
+  }
+  h3 {
+    color: $button-color;
+    font-style: italic;
+  }
+  li {
+    line-height: 2.5;
+  }
+  button {
+    border: 1px solid $button-color;
+    color: #fff;
+    font-size: 13px;
+    padding: 10px;
+    background: none;
+    &:hover {
+      background-color: $button-color;
+      color: #000;
+    }
+  }
+}
+
+@media screen and (max-width: 576px) {
+  .main-programs {
+    display: flex;
+    flex-direction: column;
+    gap:3rem;
+  }
+
+  .programs{
+    width: 100%;
+
+    h3{
+      font-size: 20px;
+    }
+    li{
+      font-size: 15px;
+    }
+  }
+}
+</style>
--- pages/membership.vue
+++ pages/membership.vue
@@ -1,5 +1,34 @@
+
+<script setup>
+
+import Programs from '~/components/membership/Programs.vue'
+
+</script>
+
 <template>
-    <nav>
-      <Navbar/>
-    </nav>
+  <nav>
+    <Navbar />
+  </nav>
+  <main>
+    <div class="main-items p-5 mt-5">
+      <h5 class="fitzone-heading text-center mt-lg-3">
+        Here we provide you the best facilities ever
+      </h5>
+        <Programs />
+    </div>
+  </main>
+  <Footer/>
 </template>
+
+<style lang="scss" scoped>
+@import "@/assets/scss/variables";
+
+.main-items {
+  margin: 5rem 0 !important;
+  h5 {
+    color: $button-color;
+  }
+
+  
+}
+</style>
